37c014637bf40c04383778f052ea6b216429a27c
HackJack14 <Juri05@gmx.net>
2025-01-22 18:17:36 +0100

    rendering a list of all games managed by SteamSync

--- client.go
+++ client.go
@@ -1,77 +1,7 @@
 package main
 
-import (
-	"log"
-	"net"
-	"os"
-	"path"
-
-	"github.com/HackJack14/SteamSync/network"
-)
+import "github.com/HackJack14/SteamSync/window"
 
 func main() {
-	if os.Args[1] == "1" {
-		listen()
-	} else {
-		send()
-	}
-}
-
-func listen() {
-	listener, err := net.Listen("tcp", "127.0.0.1:9999")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer listener.Close()
-
-	conn, err := listener.Accept()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer conn.Close()
-
-	connection := network.NewConnection(&conn)
-	network.RegisterHandler()
-	packetType, err := connection.ReadPacketType()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	err = network.HandlePacket(connection, packetType)
-	if err != nil {
-		log.Fatalln(err)
-	}
-}
-
-func send() {
-	conn, err := net.Dial("tcp", "127.0.0.1:9999")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer conn.Close()
-
-	connection := network.NewConnection(&conn)
-	sender := network.NewRequestSender(connection)
-
-	// file transfer worked successfully
-	test := network.NewDownloadFileRequest()
-	test.Game = "Fallout 3"
-	test.Save = "1-Outside"
-
-	testResponse := network.NewDownloadFileResponse()
-
-	file, err := os.Create(path.Join(test.Game, test.Save))
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer file.Close()
-
-	log.Println(testResponse)
-	err = sender.SendRequestReadBinary(test.PacketType, test, testResponse, file)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	if testResponse.ErrorCode == 0 {
-		log.Println("successfully downloaded file")
-	}
+	window.RenderWindow()
 }
--- window/window.go
+++ window/window.go
@@ -3,216 +3,93 @@ package window
 import (
 	"log"
 	"os"
-	"runtime"
+	"path"
 
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/dialog"
-	"fyne.io/fyne/v2/layout"
-	"fyne.io/fyne/v2/theme"
-	"fyne.io/fyne/v2/widget"
-	"github.com/go-gl/glfw/v3.3/glfw"
+	"gioui.org/app"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/widget"
+	"gioui.org/widget/material"
 )
 
-type ClientWindowEvent func(*ClientWindow)
-
-type ClientWindow struct {
-	OkClicked     ClientWindowEvent
-	CancelClicked ClientWindowEvent
-	OnIPReceived  ClientWindowEvent
-	isDialogOpen  bool
-	app           fyne.App
-	window        fyne.Window
-	ipaddress     string
-	dialog        IPDialog
-	upTab         UploadTab
-	downTab       DownloadTab
-	contTabs      *container.AppTabs
-	btnOk         *widget.Button
-	btnCancel     *widget.Button
+type gameButton struct {
+	game   string
+	button widget.Clickable
 }
 
-type UploadTab struct {
-	labelNewGame       *widget.Label
-	entryNewGame       *widget.Entry
-	btnSearchGameSaves *widget.Button
-	labelExistingGame  *widget.Label
-	selectExistingGame *widget.Select
-	labelDirectory     *widget.Label
-	entryDirectory     *widget.Entry
-	btnOpenDialog      *widget.Button
-}
-
-type DownloadTab struct {
-	labelGame      *widget.Label
-	selectGame     *widget.Select
-	labelDirectory *widget.Label
-	entryDirectory *widget.Entry
-	btnOpenDialog  *widget.Button
-}
-
-type IPDialog struct {
-	labelIPAddress *widget.Label
-	entryIPAddress *widget.Entry
-	btnOk          *widget.Button
-	btnCancel      *widget.Button
-}
-
-func NewClientWindow() *ClientWindow {
-	return &ClientWindow{}
-}
-
-func (cla *ClientWindow) Init() {
-	cla.app = app.New()
-	cla.app.Settings().SetTheme(theme.DarkTheme())
-	setScaling()
-	cla.window = cla.app.NewWindow("SteamSync Client")
-	cla.window.Resize(fyne.NewSize(500, 200))
-	cla.window.SetMaster()
-
-	cla.dialog.labelIPAddress = widget.NewLabel("Input your IP-Address:")
-	cla.dialog.entryIPAddress = widget.NewEntry()
-	cla.dialog.entryIPAddress.OnSubmitted = func(s string) { cla.dialog.okClicked(cla) }
-	cla.dialog.btnOk = widget.NewButton("OK", func() { cla.dialog.okClicked(cla) })
-	cla.dialog.btnCancel = widget.NewButton("Cancel", func() { cla.dialog.cancelClicked(cla) })
-	contBtns := container.NewHBox(cla.dialog.btnOk, cla.dialog.btnCancel)
-	contMain := container.NewVBox(cla.dialog.labelIPAddress, cla.dialog.entryIPAddress, contBtns)
-	cla.window.SetContent(contMain)
-	cla.isDialogOpen = true
-
-	cla.downTab.labelGame = widget.NewLabel("Game:")
-	cla.downTab.selectGame = widget.NewSelect([]string{}, func(s string) {})
-	cla.downTab.labelDirectory = widget.NewLabel("Save Directory")
-	cla.downTab.entryDirectory = widget.NewEntry()
-	cla.downTab.btnOpenDialog = widget.NewButton("...", func() { cla.openDirDialog(cla.downTab.entryDirectory) })
-
-	cla.upTab.labelNewGame = widget.NewLabel("New Game:")
-	cla.upTab.entryNewGame = widget.NewEntry()
-	cla.upTab.btnSearchGameSaves = widget.NewButton("Search", func() { cla.searchGameSaves(cla.upTab.entryNewGame.Text, cla.upTab.entryDirectory) })
-	cla.upTab.labelExistingGame = widget.NewLabel("Existing Game")
-	cla.upTab.selectExistingGame = widget.NewSelect([]string{}, func(s string) {})
-	cla.upTab.labelDirectory = widget.NewLabel("Save Directory")
-	cla.upTab.entryDirectory = widget.NewEntry()
-	cla.upTab.btnOpenDialog = widget.NewButton("...", func() { cla.openDirDialog(cla.upTab.entryDirectory) })
-
-	cla.btnOk = widget.NewButton("Ok", func() { cla.OkClicked(cla) })
-	cla.btnCancel = widget.NewButton("Cancel", func() { cla.CancelClicked(cla) })
-}
-
-func (cla *ClientWindow) Show() {
-	cla.window.ShowAndRun()
-}
-
-func (cla *ClientWindow) Close() {
-	cla.window.Close()
-	cla.app.Quit()
-}
-
-func (cla *ClientWindow) openDirDialog(outputEntry *widget.Entry) {
-	dialog.ShowFolderOpen(func(uri fyne.ListableURI, err error) {
+func RenderWindow() {
+	go func() {
+		window := new(app.Window)
+		err := run(window)
 		if err != nil {
-			log.Fatalln(err)
+			log.Fatal(err)
 		}
-
-		if uri != nil {
-			outputEntry.SetText(uri.Path())
-		}
-	}, cla.window)
+		os.Exit(0)
+	}()
+	app.Main()
 }
 
-func (cla *ClientWindow) searchGameSaves(game string, output *widget.Entry) {
-	windows, linux, err := internal.GetSaves(game)
+func readGames() ([]string, error) {
+	entries, err := os.ReadDir(path.Dir(os.Args[0]))
 	if err != nil {
-		log.Println(err)
+		return []string{}, err
 	}
 
-	switch runtime.GOOS {
-	case "windows":
-		output.SetText(internal.BuildWindowsPath(windows))
-	case "linux":
-		output.SetText(internal.BuildSteamDeckPath(linux, windows))
-	}
-}
-
-func setScaling() {
-	var actScreenWidth int
-
-	err := glfw.Init()
-	if err != nil {
-		actScreenWidth = 100
-	}
-	defer glfw.Terminate()
-
-	monitor := glfw.GetPrimaryMonitor()
-	actScreenWidth, _ = monitor.GetPhysicalSize()
-
-	if actScreenWidth <= 100 {
-		os.Setenv("FYNE_SCALE", "0.2")
-	} else if actScreenWidth > 100 && actScreenWidth <= 200 {
-		os.Setenv("FYNE_SCALE", "0.75")
-	} else {
-		os.Setenv("FYNE_SCALE", "1")
-	}
-}
-
-func (cla *ClientWindow) SetGames(games []string) {
-	cla.upTab.selectExistingGame.Options = games
-	cla.downTab.selectGame.Options = games
-}
-
-func (cla *ClientWindow) GetIP() string {
-	return cla.ipaddress
-}
-
-func (cla *ClientWindow) GetRequest() string {
-	if cla.contTabs.Selected().Text == "Upload" {
-		return "UPLOAD"
-	} else {
-		return "DOWNLOAD"
-	}
-}
-
-func (cla *ClientWindow) GetGame() string {
-	if cla.contTabs.Selected().Text == "Upload" {
-		if cla.upTab.entryNewGame.Text != "" {
-			return cla.upTab.entryNewGame.Text
-		} else {
-			return cla.upTab.selectExistingGame.Selected
+	games := make([]string, 0)
+	for _, entry := range entries {
+		if entry.IsDir() {
+			games = append(games, entry.Name())
 		}
-	} else {
-		return cla.downTab.selectGame.Selected
+	}
+	return games, nil
+}
+
+var (
+	gamesList   []string
+	buttonsList []gameButton
+)
+
+func run(window *app.Window) error {
+	theme := material.NewTheme()
+	gamesList, err := readGames()
+	buttonsList = make([]gameButton, 0)
+	for _, game := range gamesList {
+		button := gameButton{
+			game:   game,
+			button: widget.Clickable{},
+		}
+		buttonsList = append(buttonsList, button)
+	}
+	if err != nil {
+		return err
+	}
+	var ops op.Ops
+	for {
+		switch e := window.Event().(type) {
+		case app.DestroyEvent:
+			return e.Err
+		case app.FrameEvent:
+			gtx := app.NewContext(&ops, e)
+
+			renderFirst(gtx, theme)
+
+			e.Frame(gtx.Ops)
+		}
 	}
 }
 
-func (cla *ClientWindow) GetDir() string {
-	if cla.contTabs.Selected().Text == "Upload" {
-		return cla.upTab.entryDirectory.Text
-	} else {
-		return cla.downTab.entryDirectory.Text
+func renderFirst(gtx layout.Context, theme *material.Theme) {
+	list := layout.List{
+		Axis:        layout.Vertical,
+		ScrollToEnd: false,
+		Alignment:   layout.Middle,
 	}
+	list.Layout(gtx, len(gamesList), func(gtx layout.Context, index int) layout.Dimensions {
+		return material.Button(theme, &buttonsList[index].button, buttonsList[index].game).Layout(gtx)
+	})
 }
 
-func (dialog *IPDialog) okClicked(cla *ClientWindow) {
-	cla.ipaddress = dialog.entryIPAddress.Text
-
-	contBtns := container.NewHBox(cla.btnOk, cla.btnCancel)
-
-	contNewGame := container.NewStack(cla.upTab.entryNewGame, container.NewHBox(layout.NewSpacer(), cla.upTab.btnSearchGameSaves))
-	contFolderOpenUpload := container.NewStack(cla.upTab.entryDirectory, container.NewHBox(layout.NewSpacer(), cla.upTab.btnOpenDialog))
-	contUpload := container.NewVBox(cla.upTab.labelNewGame, contNewGame, cla.upTab.labelExistingGame, cla.upTab.selectExistingGame, cla.upTab.labelDirectory, contFolderOpenUpload)
-
-	contFolderOpenDownload := container.NewStack(cla.downTab.entryDirectory, container.NewHBox(layout.NewSpacer(), cla.downTab.btnOpenDialog))
-	contDownload := container.NewVBox(cla.downTab.labelGame, cla.downTab.selectGame, cla.downTab.labelDirectory, contFolderOpenDownload)
-
-	cla.contTabs = container.NewAppTabs(container.NewTabItem("Upload", contUpload), container.NewTabItem("Download", contDownload))
-	contMain := container.NewVBox(cla.contTabs, contBtns)
-	cla.window.SetContent(contMain)
-	cla.isDialogOpen = false
-
-	cla.OnIPReceived(cla)
-}
-
-func (dialog *IPDialog) cancelClicked(cla *ClientWindow) {
-	cla.Close()
+func renderSecond(gtx layout.Context, theme *material.Theme, game string) {
+	// downloadButton := new(widget.Clickable)
+	// uploadButton := new(widget.Clickable)
 }
